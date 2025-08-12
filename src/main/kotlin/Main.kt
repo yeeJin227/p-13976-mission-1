469736eba2de8e23be2b5dcb2e409068928b887c
yeeJin227 <dpwls99600@gmail.com>
2025-08-12 11:15:12 +0900

    step2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,8 +10,13 @@ fun main() {
 
         val input = readlnOrNull()!!.trim()
 
-        if (input == "종료"){
+        if (input == "종료")
             break
+        else if (input == "등록"){
+            print("명언) ")
+            val content = readlnOrNull()!!.trim()
+            print("작가) ")
+            val author = readlnOrNull()!!.trim()
         }
     }
 }
